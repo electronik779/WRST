27aa23e7b1234dafca6f87713d4630d8735d7172
Голубев Андрей <GolubevAV@whitecoal.ru>
2026-03-13 19:21:07 +0300

    Собран интерфейс ввода. Реализована проверка корректности вводимых данных. Реализовано сохранение введенных данных.

--- WRST.maui/MauiProgram.cs
+++ WRST.maui/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace WRST.maui
 {
@@ -9,6 +10,7 @@ namespace WRST.maui
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
