fe8be519aa8de24a2a1f2d154379fb53486fcd7c
Голубев Андрей <GolubevAV@whitecoal.ru>
2026-06-25 17:23:33 +0300

    Адаптировано к темной теме

--- WRST.maui/TableRow.cs
+++ WRST.maui/TableRow.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using Microsoft.Maui.Graphics; // Для Color в .NET 6+
-using Microsoft.Maui.Controls;
 
 namespace WRST.maui
 {
