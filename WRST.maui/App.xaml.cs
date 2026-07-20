31b5396918edc3ff2207d129aba8970948988d8e
Andre <electronik@inbox.ru>
2026-07-20 20:10:06 +0300

    Update App.xaml.cs

--- WRST.maui/App.xaml.cs
+++ WRST.maui/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿#if MACCATALYST
-    using AppKit;
-#endif
-
-namespace WRST.maui
+﻿namespace WRST.maui
 {
     public partial class App : Application
     {
@@ -20,42 +16,6 @@ namespace WRST.maui
             window.Width = 800;
             window.Height = 800;
 
-#if MACCATALYST
-            window.Created += (s, e) =>
-            {
-                var nativeWindow = window.Handler?.PlatformView as NSWindow;
-                if (nativeWindow != null)
-                {
-                    nativeWindow.WillClose += async (sender, args) =>
-                    {
-                        var mainPage = MainPage.Current;
-                        if (mainPage == null || mainPage.IsSaved)
-                            return;
-
-                        var alert = new NSAlert()
-                        {
-                            AlertStyle = NSAlertStyle.Warning,
-                            MessageText = "Несохранённые данные",
-                            InformativeText = "Вы хотите сохранить изменения перед выходом?",
-                        };
-                        alert.AddButton("Да");
-                        alert.AddButton("Нет");
-
-                        var result = alert.RunModal();
-
-                        if (result == 1000) // "Да"
-                        {
-                            await mainPage.SaveDataAsync();
-                            NSApplication.SharedApplication.Terminate(NSApplication.SharedApplication.Delegate);
-                        }
-                        else if (result == 1001) // "Нет"
-                        {
-                            NSApplication.SharedApplication.Terminate(NSApplication.SharedApplication.Delegate);
-                        }
-                    };
-                }
-            };
-#endif
             return window;
         }
     }
