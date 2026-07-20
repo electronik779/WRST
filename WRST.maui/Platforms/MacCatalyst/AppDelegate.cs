84634532a381c532c59812db7d29e157522a5306
Andre <electronik@inbox.ru>
2026-07-20 20:26:39 +0300

    Update AppDelegate.cs

--- WRST.maui/Platforms/MacCatalyst/AppDelegate.cs
+++ WRST.maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,10 +1,29 @@
-﻿using Foundation;
+#if MACCATALYST
+using AppKit;
+using Foundation;
 
-namespace WRST.maui
+namespace WRST.maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
 {
-    [Register("AppDelegate")]
-    public class AppDelegate : MauiUIApplicationDelegate
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    // Статический конструктор вызывается при старте приложения на Mac
+    static AppDelegate()
     {
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+        // Подменяем делегат нативного NSApplication на наш кастомный класс
+        NSApplication.SharedApplication.Delegate = new MacCatalystAppDelegate();
     }
 }
+
+// Нативный класс-делегат из мира AppKit (macOS)
+public class MacCatalystAppDelegate : NSApplicationDelegate
+{
+    // Предотвращает автоматическое закрытие процесса при закрытии последнего окна
+    public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender)
+    {
+        return false; 
+    }
+}
+#endif
